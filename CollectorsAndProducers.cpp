fd4bb2b33c75691e42d85be63be08621355cb59e
Drasungor <aguscambiano@gmail.com>
2020-05-08 16:36:57 -0300

    Added file opening

---
+++ CollectorsAndProducers.cpp
@@ -0,0 +1,31 @@
+#include "CollectorsAndProducers.h"
+
+//#include <iostream>
+#include <fstream>
+
+#define NUMBER_OF_ARGUMENTS 3
+#define MATERIALS_FILE_INDEX 1
+#define WORKERS_FILE_INDEX 2
+
+#define SUCCESS 0
+#define INVALID_ARGUMENTS 1
+#define INVALID_FILE 1
+CollectorsAndProducers::CollectorsAndProducers(){
+}
+
+CollectorsAndProducers::~CollectorsAndProducers(){
+}
+
+
+
+int execute(const char** arguments, int number_of_arguments){
+  if (number_of_arguments != NUMBER_OF_ARGUMENTS) {
+    return INVALID_ARGUMENTS;
+  }
+  std::ifstream materials(arguments[MATERIALS_FILE_INDEX]);
+  std::ifstream workers(arguments[WORKERS_FILE_INDEX]);
+  if ((materials.is_open()) || (workers.is_open())) {
+    return INVALID_FILE;
+  }
+  return SUCCESS;
+}
---
+++ CollectorsAndProducers.h
@@ -0,0 +1,16 @@
+#ifndef _COLLECTORSANDPRODUCERS_H_
+#define _COLLECTORSANDPRODUCERS_H_
+
+
+class CollectorsAndProducers {
+private:
+
+public:
+  CollectorsAndProducers();
+
+	~CollectorsAndProducers();
+
+  int execute(const char** arguments, int number_of_arguments);
+};
+
+#endif
