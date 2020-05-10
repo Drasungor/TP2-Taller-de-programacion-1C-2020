7964680ab3be8feca2581cb3fa417bae12d012ef
Drasungor <aguscambiano@gmail.com>
2020-05-10 01:48:14 -0300

    Declared map for the queues

--- CollectorsAndProducers.cpp
+++ CollectorsAndProducers.cpp
@@ -3,6 +3,9 @@
 //#include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include "Resource.h"
+#include "BlockingQueue.h"
 
 #define NUMBER_OF_ARGUMENTS 3
 #define MATERIALS_FILE_INDEX 1
@@ -26,6 +29,8 @@ int execute(const char** arguments, int number_of_arguments){
   if (number_of_arguments != NUMBER_OF_ARGUMENTS) {
     return INVALID_ARGUMENTS;
   }
+  //PASAR A FUNCION A PARTE DONDE SE INICIALICEN LOS GATHERERS
+  //O PRODUCERS
   std::ifstream materials(arguments[MATERIALS_FILE_INDEX]);
   std::ifstream workers(arguments[WORKERS_FILE_INDEX]);
   if ((!materials.is_open()) || (!workers.is_open())) {
@@ -33,7 +38,7 @@ int execute(const char** arguments, int number_of_arguments){
   }
 
   //ACA SE TIRAN LOS THREADS DE RECOLECTORES
-
+  std::map<Resource, BlockingQueue> gatherers_queues;
   //esto se hace despues de cargar los chars en las colas
   load_resources(materials);
 
